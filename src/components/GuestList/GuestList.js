5d180821dac2b00a7af716a2bdc3546b976c8bad
Robyn Noble <robynhurd09@gmail.com>
2021-04-28 16:52:41 -0500

    guestList added

---
+++ src/components/GuestList/GuestList.js
@@ -0,0 +1,27 @@
+
+function GuestList(props){
+    console.log('GuestList props are:', props);
+    return(
+        <>
+        <h2>Guest List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Kid's Meal</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.guestList.map(guest => (
+            <tr key={guest.id}>
+              <td>{guest.name}</td>
+              <td>{String(guest.kidsMeal)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </>
+    )
+}
+
+export default GuestList;
