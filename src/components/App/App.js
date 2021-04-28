38614c35d38b9613a7c46a00d8080eae861500b2
Robyn Noble <robynhurd09@gmail.com>
2021-04-28 16:58:53 -0500

    dinnerSupplies updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import GuestList from '../GuestList/GuestList';
+import DinnerSupplies from '../DinnerSupplies/DinnerSupplies';
 
 import './App.css';
-
 
 function App() {
   let [guestList, setGuestList] = useState([]);
@@ -105,16 +105,7 @@ function App() {
         <button type="submit">Add Guest</button>
       </form>
       <GuestList guestList={guestList} />
-      <h2>Dinner Supplies</h2>
-      <div>
-        Spoons: {guestList.length * 2}
-      </div>
-      <div>
-        Forks: {guestList.length * 2}
-      </div>
-      <div>
-        Knives: {guestList.length * 2}
-      </div>
+      <DinnerSupplies guestList={guestList} />
       <Footer />
     </div>
   );
